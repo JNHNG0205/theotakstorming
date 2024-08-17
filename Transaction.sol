e265107854ea739958ca6235de47fcd77fcdf0d0
JNHNG0205 <heng.teh0205@gmail.com>
2024-08-17 14:13:18 +0800

    changes

--- Transaction.sol
+++ Transaction.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.0;
 
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 contract SimpleTransaction {
     // Mapping to store the balance of each address
     mapping(address => uint256) public balances;
